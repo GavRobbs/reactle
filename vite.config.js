22b4d3f6e9d795ec4becb78eaa52565947639304
Gavin <gnation15@gmail.com>
2025-01-23 15:48:36 -0500

    Updated the base in the vite config.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/reactle/",
   build: {
-    outDir: 'reactle',
+    outDir: 'reactle', //Thats what I call it on the server
   }
 })
