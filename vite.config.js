7191265eb7f03fb53ed83774d64716c236cab5ba
Gavin <gnation15@gmail.com>
2025-01-23 15:29:26 -0500

    Fixed the config file.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'reactle', //Thats what I call it on the server
+    outDir: 'reactle',
   }
 })
